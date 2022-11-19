7749c73013907e5eff24e9eab337b66a2a66685f
julia-kheil <115854674+julia-kheil@users.noreply.github.com>
2022-11-19 09:44:21 +0700

    Add files via upload

---
+++ variabel.java
@@ -0,0 +1,10 @@
+public class variabel{
+    public static void main(String[] args) {
+String nama = "julia";
+		int tahun	= 2003;
+		System.out.println("Assalamu'alaikum");
+		System.out.println("Nama saya : " + nama);
+		System.out.println("Saya lahir di tahun : " + tahun);
+
+    }
+}
